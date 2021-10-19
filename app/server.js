808b03ec83c1ec0954bc2c1cc3cf90c895218203
Martin WEB. Larsson <20martin.larsson@glimnet.se>
2021-10-19 14:41:23 +0200

    feat: broadcast to dedicated user implemented
    function which allows for broadcast in websockets via ids functioning.

--- app/server.js
+++ app/server.js
@@ -22,7 +22,7 @@ import {
     formatToStatusObj
 } from './utilities/messages.js';
 import {
-  botWelcomeMessage
+    botWelcomeMessage
 } from './utilities/botmessages.js';
 import {
     getCollectionOfGallery,
@@ -92,9 +92,9 @@ let clientsArray = []
 wss.on('connection', async (ws, req) => {
     ws.id = tempIdBecauseSessionHatesWebsockets;
     console.log(`Client connected from IP ${ws._socket.remoteAddress}`);
-   
+
     // console.log("hello from server websocket", await botWelcomeMessage(ws.id));
-  
+
     broadcast(await botWelcomeMessage(ws.id));
 
     let clientSize = formatToStatusObj("status", "clientInteger", wss.clients.size)
@@ -117,6 +117,8 @@ wss.on('connection', async (ws, req) => {
 
         let BotGoodbyeMsg = formatToChatObj("botMsg", "Mr Bot", "req.user.name... left the the chat!")
         broadcast(validateTypeOfOutgoingMessage(BotGoodbyeMsg));
+
+
     });
 
     ws.on("message", (data) => {
@@ -128,12 +130,16 @@ wss.on('connection', async (ws, req) => {
         // if (validatedOutgoingMsg === "error") {
         //     // TODO Broadcast to user only that their message didn't og through!
         // }
+        // let onlyYou = formatToChatObj("botMsg", "Mr Bot", "Only you should see this!")
+        // broadcastToSingleClient(JSON.stringify(onlyYou), ws.id)
+
         broadcast(validatedOutgoingMsg, ws);
     })
 });
 
 function broadcastButExclude(data, someClient) {
     wss.clients.forEach(function each(client) {
+        console.log("hello", client);
         if (client.readyState === WebSocket.OPEN) {
             if (client !== someClient) {
                 client.send(data);
@@ -142,91 +148,97 @@ function broadcastButExclude(data, someClient) {
     });
 }
 
-function broadcastToSingleClient() {
-    
-}
-
-function broadcast(data) {
-    wss.clients.forEach(client => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(data)
-        }
-    })
-}
-
-app.get("/", checkUserAccess, (req, res) => {
-    res.render('pages/index');
-})
-
-app.get('/logout', checkUserAccess, (req, res) => {
-    req.session.destroy((err) => {
-        if (err) {
-            console.log(err);
-            res.redirect('/')
-            return
-        }
-        res.clearCookie(SESSION_NAME);
-        console.log('cookie destroyed');
-        res.redirect('/')
-    });
-})
-
-app.get("/login/", (req, res) => {
-    res.render('pages/login');
-})
-
-app.post("/login/", async (req, res) => {
-    console.log(req.body);
-    const {
-        userName,
-        userPassword
-    } = req.body;
-    const user = await loginUser(userName, userPassword);
-    if (user === "failure") {
-        res.json("There ain't no user here :<");
-        return;
+function broadcastToSingleClient(data, specificUserId) {
+    wss.clients.forEach(function each(client) {
+                if (client.readyState === WebSocket.OPEN) {
+                    if (client.id === specificUserId) {
+                        client.send(data);
+                    }
+                }
+            });
     }
-    req.session.userHasAccess = true;
-    const updatedUser = await setIdAndStatusForWebsocket(user);
-    if (updatedUser === "failure") {
-        res.json("Couldn't set new stats");
-        return;
-    }
-    tempIdBecauseSessionHatesWebsockets = updatedUser.tempWebsocketId;
-    res.json({
-        redirectTo: '/',
-        message: "user exist and logging in!"
-    })
-})
 
-
-app.get("/register/", (req, res) => {
-    res.render('pages/register');
-})
-
-app.post("/register/", async (req, res) => {
-    const {
-        userName,
-        userPassword
-    } = req.body;
-    const newUser = await registerNewUser(userName, userPassword);
-    console.log(newUser);
-    if (newUser === "success") {
-        res.json({
-            redirectTo: '/login',
-            message: "new user added, log in!"
+    function broadcast(data) {
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(data)
+            }
         })
-        return;
     }
-    res.json("New user did not add :<");
-})
 
-app.get("/gallery/", async (req, res) => {
-    const collection = await getCollectionOfGallery();
-    // TODO put it into an object and error handle!
-    res.json(collection);
-})
+    app.get("/", checkUserAccess, (req, res) => {
+        res.render('pages/index');
+    })
 
-server.listen(process.env.PORT || PORT, () => {
-    console.log(`Server started on`, PORT);
-});
+    app.get('/logout', checkUserAccess, (req, res) => {
+        req.session.destroy((err) => {
+            if (err) {
+                console.log(err);
+                res.redirect('/')
+                return
+            }
+            res.clearCookie(SESSION_NAME);
+            console.log('cookie destroyed');
+            res.redirect('/')
+        });
+    })
+
+    app.get("/login/", (req, res) => {
+        res.render('pages/login');
+    })
+
+    app.post("/login/", async (req, res) => {
+        console.log(req.body);
+        const {
+            userName,
+            userPassword
+        } = req.body;
+        const user = await loginUser(userName, userPassword);
+        if (user === "failure") {
+            res.json("There ain't no user here :<");
+            return;
+        }
+        req.session.userHasAccess = true;
+        const updatedUser = await setIdAndStatusForWebsocket(user);
+        if (updatedUser === "failure") {
+            res.json("Couldn't set new stats");
+            return;
+        }
+        tempIdBecauseSessionHatesWebsockets = updatedUser.tempWebsocketId;
+        res.json({
+            redirectTo: '/',
+            message: "user exist and logging in!"
+        })
+    })
+
+
+    app.get("/register/", (req, res) => {
+        res.render('pages/register');
+    })
+
+    app.post("/register/", async (req, res) => {
+        const {
+            userName,
+            userPassword
+        } = req.body;
+        const newUser = await registerNewUser(userName, userPassword);
+        console.log(newUser);
+        if (newUser === "success") {
+            res.json({
+                redirectTo: '/login',
+                message: "new user added, log in!"
+            })
+            return;
+        }
+        res.json("New user did not add :<");
+    })
+
+    app.get("/gallery/", async (req, res) => {
+        const collection = await getCollectionOfGallery();
+        // TODO put it into an object and error handle!
+        res.json(collection);
+    })
+
+    server.listen(process.env.PORT || PORT, () => {
+        console.log(`Server started on`, PORT);
+    });
